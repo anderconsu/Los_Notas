e2364782528e18c4d0f2392a8a350e04d59fb149
anderconsu <anderconsu@gmail.com>
2023-07-06 17:55:26 +0200

    type modules, testeo added, note and layout pubs

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+app.use(express.static('public'))
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+app.get('/', (req, res) => {
+    res.render('testeo');
+})
+app.listen(3001,()=>{console.log("servidor en marcha en el puerto 3001")});
