390bc2112981d2d100c06e22836d2b137adf5482
anderconsu <anderconsu@gmail.com>
2023-07-08 11:12:47 +0200

    - App and DB dockerized (ERROR in database insert) - DB folders changed - Index's console log modified yo show external port - DEV version

--- index.js
+++ index.js
@@ -8,4 +8,4 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('testeo');
 })
-app.listen(3001,()=>{console.log("servidor en marcha en el puerto 3001")});
+app.listen(3000,()=>{console.log(`servidor en marcha en el puerto 3000 del container, en puerto ${process.env.PORT} EXTERNO`)});
