baf1e5f7efbaed1d05fe3537be20e351b187c1a3
DPachoGit <davidpacho1513@gmail.com>
2023-07-08 10:34:53 +0200

    Routes creados

---
+++ routes/home.js
@@ -0,0 +1,9 @@
+import express from "express";
+import homeController from "../controllers/homeController.js";
+
+const router = express.Router();
+
+// Ruta de la página principal
+router.get("/", homeController.index);
+
+export default router;
