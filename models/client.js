6a296ced1cf77a21549f9107651bd827c0b37329
DPachoGit <davidpacho1513@gmail.com>
2023-07-08 10:33:26 +0200

    Models creados

---
+++ models/client.js
@@ -0,0 +1,32 @@
+import connection from "../config/sequelize.js";
+import Sequelize from "sequelize";
+
+const Client = connection.define(
+  "client",
+  {
+    id: {
+      type: Sequelize.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    name: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    lastname: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: Sequelize.STRING(45),
+      allowNull: false,
+    },
+  },
+  { indexes: [{ unique: true, fields: ["username"] }] }
+);
+
+export default Client;
