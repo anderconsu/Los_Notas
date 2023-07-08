e0ccc8f71b35e71cf0d9d74c9b346d4524bd2e68
anderconsu <anderconsu@gmail.com>
2023-07-08 11:26:53 +0200

    dontev added to packages, invocked in index.js

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.static('public'))
@@ -8,4 +10,4 @@ app.set('view engine', 'pug');
 app.get('/', (req, res) => {
     res.render('testeo');
 })
-app.listen(3000,()=>{console.log(`servidor en marcha en el puerto 3000 del container, en puerto ${process.env.PORT} EXTERNO`)});
+app.listen(3000,()=>{console.log(`servidor en marcha en el puerto 3000 del container, en puerto ${process.env.APP_PORT} EXTERNO`)});
