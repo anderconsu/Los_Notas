fa3b69f0bf2b27dbd9330b3927c938a3947d7eb1
anderconsu <anderconsu@gmail.com>
2023-07-15 12:40:45 +0200

    client error management done

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -1,134 +1,132 @@
 import Client from "../models/client.js";
-import Sequelize from "sequelize";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 class ClientController {
-  async createClientView(req, res){
-    res.render("client/signup");
-  }
-  async createClient(req, res) {
-      try {
-        let { username, name, lastname, password, password_repeat } = req.body;
-        if (password !== password_repeat) {
-          let errorItem = new Sequelize.ValidationErrorItem({
-            message: "Las contraseñas no coinciden, joder!",
-            path: "password",
-            type: "Validation error",
-          });
-          throw new Sequelize.ValidationError("", [errorItem]);
-        }
-        let hash = await bcrypt.hash(password, 10);
-
-        const client = await Client.create({
-          username,
-          name,
-          lastname,
-          password:hash,
-        });
-        res.status(201).redirect("/client/login");
-      } catch (error) {
-        if (error instanceof Sequelize.ValidationError) {
-          console.log("errors", error.errors);
-          if (error.errors.length > 0) {
-            if (error.errors[0].type === "notNull Violation") {
-              error.message = "Todos los campos son obligatorios";
-            } else if (error.errors[0].type === "unique violation") {
-              error.message = "El usuario ya existe";
-            } else if (error.errors[0].type === "Validation error") {
-              error.message = error.errors[0].message;
+    async createClientView(req, res) {
+        res.render("client/signup");
+    }
+    async createClient(req, res) {
+        try {
+            let { username, name, lastname, password, password_repeat } =
+                req.body;
+            if (password !== password_repeat) {
+                throw new Error("Passwords do not match");
             }
-          } else {
-            console.log("message", error.message);
-          }
-        } else {
-          error.message = "Error desconocido al registrarse";
+            const existingClient = await Client.findOne({ username });
+            if (existingClient) {
+                throw new Error("The username is already taken");
+            }
+            let hash = await bcrypt.hash(password, 10);
+            const client = await Client.create({
+                username,
+                name,
+                lastname,
+                password: hash,
+            });
+            res.status(201).redirect("/client/login");
+        } catch (error) {
+            console.error("Error creating client:", error);
+            // Handle specific errors and send appropriate responses
+            if (error.message === "Passwords do not match") {
+                res.status(400).json({ error: "Passwords do not match" });
+            } else if (error.message === "The username is already taken") {
+                res.status(409).json({
+                    error: "The username is already taken",
+                });
+            } else {
+                res.status(500).json({ error: "Internal Server Error" });
+            }
         }
-        
-      }
-  }
-  async createClientApi(req, res){    
-    try {
-      let { username, password, password_repeat } = req.body;
-      if (password !== password_repeat) {
-        return res.status(400).json({
-          error: "Las contraseñas no coinciden",
-        });
-      }
-      let hash = await bcrypt.hash(password, 11);
-      let result = await Usuario.create({ username, password: hash });
-      return res.status(200).json(result);
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        error: "Error desconocido al registrarse",
-      });
     }
-  }
-  async createLoginView(req, res){
-    res.render("client/login");
-  }
-  async verifyLogin (req, res){
-    try {
-      let { username, password } = req.body;
-      const client = await Client.findOne({
-        where: {
-          username,
-        },
-      })
-      if (!client){
-        throw new Error("No existe el usuario");
-      }else{
-        console.log(client);
-        if (await bcrypt.compare(password, client.password)) {
-          // Guardar la sesión
-          req.session.client = client;
-          // Redirigir al usuario
-          res.redirect("/");
-        } else {
-          throw new Error("Contraseña incorrecta");
+    async createClientApi(req, res) {
+        try {
+            let { username, password, password_repeat } = req.body;
+            if (password !== password_repeat) {
+                return res.status(400).json({
+                    error: "Las contraseñas no coinciden",
+                });
+            }
+            let hash = await bcrypt.hash(password, 11);
+            let result = await Usuario.create({ username, password: hash });
+            return res.status(200).json(result);
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                error: "Error desconocido al registrarse",
+            });
         }
-
-      }
-    }catch (error) {
-      console.log(error)
-      res.status(401).redirect("/client/signup");
     }
-  }
-  async verifyLoginApi (req, res){
-    console.log(req.body);
-    try {
-      let { username, password } = req.body;
-      let client = await Client.findOne({ where: { username } });
-      if (!client) {
-        return res.status(400).json({
-          error: "El usuario no existe",
-        });
-      }
-      let hash = client.password;
-      let iguales = await bcrypt.compare(password, hash);
-      console.log(iguales);
-      if (!iguales) {
-        return res.status(401).json({
-          error: "La contraseña no es correcta",
-        });
-      }
-
-      // Generar token JWT
-      let payload = { username };
-      let token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
-
-      return res.status(200).json({
-        token,
-      });
-    } catch (error) {
-      console.log(error);
-      return res.status(500).json({
-        error: "Error desconocido al iniciar sesión",
-      });
+    async createLoginView(req, res) {
+        res.render("client/login");
     }
-  }
-/*   updateClient() {
+    async verifyLogin(req, res) {
+        try {
+            let { username, password } = req.body;
+            const client = await Client.findOne({
+                where: {
+                    username,
+                },
+            });
+            if (!client) {
+                throw new Error("User or password not corrrect");
+            } else {
+                if (await bcrypt.compare(password, client.password)) {
+                    // Guardar la sesión
+                    req.session.client = client;
+                    // Redirigir al usuario
+                    res.redirect("/");
+                } else {
+                    throw new Error("User or password not corrrect");
+                }
+            }
+        } catch (error) {
+            console.log(error);
+            if (error.message === "User or password not corrrect") {
+                res.status(401).redirect("/client/login");
+            } else {
+                res.status(500).json({
+                    error: "Error desconocido al iniciar sesión",
+                });
+            }
+        }
+    }
+    async verifyLoginApi(req, res) {
+        console.log(req.body);
+        try {
+            let { username, password } = req.body;
+            let client = await Client.findOne({ where: { username } });
+            if (!client) {
+                return res.status(400).json({
+                    error: "User or password not corrrect",
+                });
+            }
+            let hash = client.password;
+            let iguales = await bcrypt.compare(password, hash);
+            console.log(iguales);
+            if (!iguales) {
+                return res.status(400).json({
+                    error: "User or password not corrrect",
+                });
+            }
+
+            // Generar token JWT
+            let payload = { username };
+            let token = jwt.sign(payload, process.env.JWT_SECRET, {
+                expiresIn: "1h",
+            });
+
+            return res.status(200).json({
+                token,
+            });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                error: "Error desconocido al iniciar sesión",
+            });
+        }
+    }
+    /*   updateClient() {
     async (req, res) => {
       try {
         const { id } = req.params;
